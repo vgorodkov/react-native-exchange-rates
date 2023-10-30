c16bb022a7856ca92ce61478e2b2c6df7a36d0b7
Victor <gorodkov.victor@gmail.com>
2023-10-30 20:24:21 +0300

    constants currencies

---
+++ src/data/currencies.ts
@@ -0,0 +1,27 @@
+export const CURRENCIES = [
+  {
+    id: 431,
+    name: 'USD',
+    img: require('../assets/us.png'),
+  },
+  {
+    id: 451,
+    name: 'EUR',
+    img: require('../assets/eu.png'),
+  },
+  {
+    id: 456,
+    name: 'RUB',
+    img: require('../assets/ru.png'),
+  },
+  {
+    id: 449,
+    name: 'UAH',
+    img: require('../assets/ua.png'),
+  },
+  {
+    id: 452,
+    name: 'PLN',
+    img: require('../assets/pl.png'),
+  },
+];
