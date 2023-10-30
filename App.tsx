9d858f0328dc26479c24ec72617b4d534067e070
Victor <gorodkov.victor@gmail.com>
2023-10-30 20:25:45 +0300

    transition between graphs, currency selection, new gesture

--- App.tsx
+++ App.tsx
@@ -1,21 +1,29 @@
 import {
   StyleSheet,
   View,
-  Text,
   useWindowDimensions,
   StatusBar,
   ActivityIndicator,
+  FlatList,
 } from 'react-native';
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 
-import {DataPoint} from './src/data/mock';
-import {Canvas, Group, Paint, Path} from '@shopify/react-native-skia';
+import {
+  Canvas,
+  Easing,
+  Group,
+  Path,
+  runTiming,
+  useComputedValue,
+  useValue,
+} from '@shopify/react-native-skia';
 import {Label} from './src/components/Label';
-import axios from 'axios';
+
 import {getCurrentDate, handleDate} from './src/utils/handleDate';
 import {makeGraph} from './src/utils/makeGraph';
 import Selection from './src/components/Selection';
 import {
+  Gesture,
   GestureDetector,
   GestureHandlerRootView,
 } from 'react-native-gesture-handler';
@@ -23,107 +31,207 @@ import Animated, {
   useAnimatedStyle,
   useDerivedValue,
   useSharedValue,
+  withTiming,
 } from 'react-native-reanimated';
 import {useGraphTouchHandler} from './src/hooks/useGraphTouchHanlder';
 import Cursor from './src/components/Cursor';
 import {getYForX} from './src/utils/math';
+import {CurrencyItem} from './src/components/CurrencyItem';
+import useCurrencyDynamicsFetch from './src/hooks/useCurrencyDynamicsFetch';
 
-const cursorSize = 42;
+import {CURRENCIES} from './src/data/currencies';
+import {fetchDataByCurrency} from './src/utils/fetchDataByCurrency';
+import {LAYOUT} from './src/constants/layout';
+import {GraphPeriods} from './src/constants/time';
+
+const CURSOR_SIZE = 42;
+
+function findClosestValue(input: number, array: number[]): number | null {
+  'worklet';
+  if (array.length === 0) {
+    return null; // Handle the case when the array is empty.
+  }
+
+  let closestValue = array[0];
+  let minDifference = Math.abs(input - closestValue);
+
+  for (let i = 1; i < array.length; i++) {
+    const currentDifference = Math.abs(input - array[i]);
+    if (currentDifference < minDifference) {
+      closestValue = array[i];
+      minDifference = currentDifference;
+    }
+  }
+
+  return closestValue;
+}
 
 const App = () => {
-  const window = useWindowDimensions();
-  const {width, height} = window;
-
-  const [isLoading, setIsLoading] = useState(true);
-  const [graphData, setGraphData] = useState<DataPoint[] | null>(null);
+  const {width, height} = useWindowDimensions();
+  const currentDate = getCurrentDate();
 
   //month/day/year
-  const [startDate, setStartDate] = useState(handleDate('1week'));
-  const [currentDate, setCurrentDate] = useState(getCurrentDate());
+  const [startDate, setStartDate] = useState(handleDate(GraphPeriods.oneWeek));
+  const [currencyId, setCurrencyId] = useState(431); //431 - default to USD
 
-  const graph = makeGraph(graphData!, width, height / 2);
+  const {data, isLoading} = useCurrencyDynamicsFetch(
+    currencyId,
+    startDate,
+    currentDate,
+  );
 
-  const dates = graphData?.map(item => item.Date);
+  const graph = data.length > 0 ? makeGraph(data, width, height / 2) : null;
+  const dates = data?.map(item => item.Date);
 
-  const x = useSharedValue(width);
+  //store current graph and next one to make transition betweeen them
+  const graphs = useValue({
+    current: graph,
+    next: graph,
+  });
+  const isTransitionCompleted = useValue(1);
+
+  //write new graph if first load or graphs period change
+  useEffect(() => {
+    if (data.length > 0) {
+      graphs.current = {
+        current: graph,
+        next: graph,
+      };
+    }
+  }, [data.length]);
+
+  const x = useSharedValue(width); //start pos = right because current date is the last one on graph
   const y = useDerivedValue(() => {
-    if (graph?.curve) {
-      return getYForX(graph.curve.toCmds(), x.value);
+    if (graphs.current.current?.curve) {
+      return getYForX(graphs.current.current.curve.toCmds(), x.value);
     } else {
       return 0;
     }
-  });
+  }, [graphs.current.current?.curve]);
 
-  const gesture = useGraphTouchHandler(x, width, graphData!?.length);
+  const gesture = useGraphTouchHandler(x, width, graph?.timeFrames);
+
   const style = useAnimatedStyle(() => {
     return {
       position: 'absolute',
-      width: cursorSize,
-      height: cursorSize,
-      left: x.value - cursorSize / 2,
-      top: y.value - cursorSize / 2 + 64 + 16, //graphPadding+canvasPadding
+      width: CURSOR_SIZE,
+      height: CURSOR_SIZE,
+      left: x.value - CURSOR_SIZE / 2,
+      top:
+        y.value -
+        CURSOR_SIZE / 2 +
+        LAYOUT.spacing.GRAPH_TOP +
+        LAYOUT.spacing.CANVAS_TOP,
     };
   });
 
-  useEffect(() => {
-    setIsLoading(true);
-    axios
-      .get(
-        `https://api.nbrb.by/ExRates/Rates/Dynamics/431?startDate=${startDate}&endDate=${currentDate}`, //change end Date to current Date. Now api not working for current Day
-      )
-      .then(({data}) => {
-        setGraphData(data);
-      })
-      .catch(e => {
-        console.log(e);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, [startDate, currentDate]);
+  const transitionChartAnimation = () => {
+    isTransitionCompleted.current = 0;
 
-  if (isLoading && !graphData) {
+    runTiming(isTransitionCompleted, 1, {
+      duration: 500,
+      easing: Easing.inOut(Easing.cubic),
+    });
+  };
+
+  const changeCurrency = useCallback(
+    async (id: number) => {
+      const newGraphData = await fetchDataByCurrency(
+        id,
+        startDate,
+        currentDate,
+      );
+      const newGraph = makeGraph(newGraphData, width, height / 2);
+      graphs.current = {
+        current: newGraph,
+        next: graphs.current.current,
+      };
+      transitionChartAnimation();
+      setCurrencyId(id);
+    },
+    [startDate],
+  );
+
+  const currentPath = useComputedValue(() => {
+    const start = graphs.current.current?.curve;
+    const end = graphs.current.next?.curve;
+    const result = start?.interpolate(end!, isTransitionCompleted.current);
+
+    return result?.toSVGString() ?? '';
+  }, [isTransitionCompleted, graphs.current.current, graphs.current]);
+
+  const newGesture = Gesture.Pan()
+    .onBegin(pos => {
+      if (graph) {
+        const dayPositions = Array.from(
+          {length: graph?.timeFrames},
+          (_, index) => (width * index) / (graph?.timeFrames - 1),
+        );
+        const closest = findClosestValue(pos.x, dayPositions);
+        x.value = withTiming(closest!, {duration: 300});
+      }
+    })
+    .onChange(e => {
+      /*  x.value = e.x; */
+    });
+
+  if (isLoading && data.length <= 0) {
     return (
-      <View
-        style={[
-          styles.container,
-          {justifyContent: 'center', alignItems: 'center'},
-        ]}>
+      <View style={[styles.container, styles.centeredView]}>
         <ActivityIndicator size={'large'} color={'cyan'} />
       </View>
     );
   }
 
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
+    <GestureHandlerRootView style={styles.root}>
       <StatusBar hidden />
       <View style={styles.container}>
-        {graph && graphData && (
-          <View style={styles.graphContainer}>
-            <Canvas style={{width: width, height: height / 2 + 64}}>
-              <Label
-                max={graph.max}
-                min={graph.min}
-                y={y}
-                x={x}
-                dates={dates}
-              />
-              <Group transform={[{translateY: 64}]}>
-                <Path
-                  style="stroke"
-                  path={graph.curve}
-                  strokeWidth={3}
-                  color="cyan"
+        {graphs.current.current && (
+          <View style={styles.canvasContainer}>
+            <GestureDetector gesture={newGesture}>
+              <Canvas
+                style={{
+                  width: width,
+                  height: height / 2 + LAYOUT.spacing.GRAPH_TOP,
+                }}>
+                <Label
+                  max={graphs.current.current.max}
+                  min={graphs.current.current.min}
+                  y={y}
+                  x={x}
+                  dates={dates}
                 />
-                <Cursor x={x} y={y} />
-              </Group>
-            </Canvas>
-            <GestureDetector gesture={gesture}>
-              <Animated.View style={[style]} />
+                <Group transform={[{translateY: LAYOUT.spacing.GRAPH_TOP}]}>
+                  <Path
+                    style="stroke"
+                    path={currentPath}
+                    strokeWidth={3}
+                    color="cyan"
+                  />
+                  <Cursor x={x} y={y} />
+                </Group>
+              </Canvas>
             </GestureDetector>
+            {/*  <GestureDetector gesture={gesture}>
+              <Animated.View style={[style]} />
+            </GestureDetector> */}
           </View>
         )}
         <Selection setStartDate={setStartDate} />
+        <FlatList
+          data={CURRENCIES}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({item}) => (
+            <CurrencyItem
+              img={item.img}
+              name={item.name}
+              id={item.id}
+              key={item.id}
+              changeCurrency={changeCurrency}
+            />
+          )}
+        />
       </View>
     </GestureHandlerRootView>
   );
@@ -132,14 +240,14 @@ const App = () => {
 export default App;
 
 const styles = StyleSheet.create({
+  root: {flex: 1},
+
   container: {
     flex: 1,
     backgroundColor: '#031926',
   },
-  graphContainer: {
-    paddingTop: 16,
+  canvasContainer: {
+    paddingTop: LAYOUT.spacing.CANVAS_TOP,
   },
-  circle: {
-    borderWidth: 1,
-  },
+  centeredView: {justifyContent: 'center', alignItems: 'center'},
 });
